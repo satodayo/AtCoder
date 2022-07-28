5a9c42af0712ee8b5fdfd2d276636e0c572ce3ae
satodayo <stakr94@gmail.com>
2022-07-28 17:05:48 +0900

    add 018

---
+++ MathAndAlgorithm/018.py
@@ -0,0 +1,20 @@
+N = int(input())
+A = list(map(int, input().split()))
+
+one = 0
+two = 0
+three = 0
+four = 0
+
+for i in range(N):
+    if A[i] == 100:
+        one += 1
+    elif A[i] == 200:
+        two += 1
+    elif A[i] == 300:
+        three += 1
+    elif A[i] == 400:
+        four = 0
+R = (one * four) + (two * three)
+
+print(R)
